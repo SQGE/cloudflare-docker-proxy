e9fa16db02511f7439db30dc9e7287cee0f62c57
Samantha Parker <30278569+SQGE@users.noreply.github.com>
2024-06-17 16:26:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+ "docker.gongbaoren.com": "https://registry-1.docker.io",
+ "quay.gongbaoren.com": "https://quay.io",
+ "gcr.gongbaoren.com": "https://gcr.io",
+ "k8s-gcr.gongbaoren.com": "https://k8s.gcr.io",
+ "k8s.gongbaoren.com": "https://registry.k8s.io",
+ "ghcr.gongbaoren.com": "https://ghcr.io",
+ "cloudsmith.gongbaoren.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
